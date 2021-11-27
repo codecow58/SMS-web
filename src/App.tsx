431e678b85710515c6a22a3737886d9152c688c0
codemonkey30 <=>
2021-11-27 08:29:38 +0530

    separation of device view done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import { ThemeProvider } from '@fluentui/react';
+import { getTheme, Icon, Stack, ThemeProvider } from '@fluentui/react';
 import { initializeIcons } from "@fluentui/font-icons-mdl2";
 import RoutesRoot from './web-view/modules/route';
+import {
+  BrowserView,
+  MobileView
+} from "react-device-detect";
 import {myTheme} from './theme'
 
 initializeIcons();
 
 export const App: React.FunctionComponent = () => {
   return (
-      <ThemeProvider theme={myTheme}>
+    <ThemeProvider theme={myTheme}>
+      <BrowserView>
         <RoutesRoot />
-      </ThemeProvider>
+      </BrowserView>
+      <MobileView>
+        <Stack
+          horizontalAlign="center"
+          verticalAlign="center"
+          styles={{ root: { minHeight: "100vh" } }}
+        >
+          <Icon
+            iconName="ConstructionConeSolid"
+            styles={{ root: { fontSize: "20vw" ,color:getTheme().palette.yellowDark} }}
+          />
+          <br />
+          <h3>we are under construction</h3>
+        </Stack>
+      </MobileView>
+    </ThemeProvider>
   );
 };
